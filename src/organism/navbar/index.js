79424c01c6a43a727b0e40f06f58be3b6fe26343
Rebecca Souza <rebecca@psafe.com>
2022-03-18 23:49:18 -0300

    structure

---
+++ src/organism/navbar/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.scss'
+
+const navbar = () => {
+    return (
+        <nav className='navbar'>
+            <div className='brand'></div>
+            <div className='menu'>
+                <div className='clip'></div>
+
+            </div>
+        </nav>
+    );
+}
+
+export default navbar;
---
+++ src/templates/publicPage/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.scss'
+
+const publicPage = () => {
+    return (
+        <div className="container">
+            <div className="carrossel">imagem</div>
+            <div className="top">top</div>
+            <div className="btn1">Jogos A-Z</div>
+            <div className="btn2">Meus Jogos</div>
+            <div className="btn3">Vídeos</div>
+        </div>
+    );
+}
+
+export default publicPage;
